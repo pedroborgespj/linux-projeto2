415d1baed1fa4420566ccf7b84c88ae4f8d870c6
Pedro Borges <pedroborgespj@gmail.com>
2023-04-14 09:45:56 -0300

    primeiro commit - script iac-projeto2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto 2 - Linux - Infraestrutura como código
+Script de provisionamento de um servidor web (apache)
+
+## Curso
+Bootcamp AWS - DIO
---
+++ script-iac2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Atualização do servidor"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+echo "Atualização concluída"
+
+echo "Baixando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+echo "Copiando os arquivos da aplicação..."
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "Fim."
